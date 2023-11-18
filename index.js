6b574f5a130a53fc8eaf569dad8847c45405368a
Ammar_coder87 <ammar.kolko87@gmail.com>
2023-11-19 01:39:24 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your code here
+function submitData( userName = 'name', userEmail = 'email' ) {
+
+    const allData = {
+        name: "Ammar",
+        email: "ammar.kolko87@gmail.com",
+      };
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(allData),
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        console.log(object);
+      })
+      .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        console.log(error.message);
+      });
+}
+submitData(userName = 'name', userEmail = 'email');
+
